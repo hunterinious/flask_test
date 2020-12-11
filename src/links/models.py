b43f79a52a988ade9f7c748749f3cc69761a941e
hunterinious <vlnerodenko@gmail.com>
2020-12-11 14:21:27 +0200

    timzeone set to False and default added

---
+++ src/links/models.py
@@ -0,0 +1,10 @@
+from src.flask_test.db import db
+from datetime import datetime, timedelta
+
+
+class Link(db.Model):
+    __tablename__ = 'links'
+    id = db.Column(db.Integer, primary_key=True)
+    short_url = db.Column(db.String(8), unique=True, nullable=False)
+    long_url = db.Column(db.String(255), nullable=False)
+    expire_date = db.Column(db.TIMESTAMP(timezone=False), nullable=False, default=datetime.now() + timedelta(days=90))
