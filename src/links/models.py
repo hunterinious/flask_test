485545ee359acec5df7dfc10f81dc03eba06608d
hunterinious <vlnerodenko@gmail.com>
2020-12-15 17:17:14 +0200

    constants added to Link model

--- src/links/models.py
+++ src/links/models.py
@@ -5,11 +5,15 @@ import os
 
 
 class Link(db.Model):
+    MAXIMAL_ALLOWED_URL_LENGTH = 2048
+    NUMBER_OF_RANDOM_BYTES_FOR_SHORT_URL = 4
+
     __tablename__ = 'links'
     id = db.Column(db.BigInteger, primary_key=True)
     short_url = db.Column(db.String(35), unique=True, nullable=False)
-    long_url = db.Column(db.String(2048), nullable=False)
-    expire_date = db.Column(db.TIMESTAMP(timezone=False), nullable=False, default=datetime.now() + timedelta(days=90))
+    long_url = db.Column(db.String(MAXIMAL_ALLOWED_URL_LENGTH), nullable=False)
+    expire_date = db.Column(db.TIMESTAMP(timezone=False), nullable=False,
+                            default=datetime.now() + timedelta(days=int(os.environ.get('DEFAULT_STORAGE_DAYS'))))
     is_expired = db.Column(db.Boolean, nullable=False, default=False)
 
     @property
@@ -28,7 +32,7 @@ class Link(db.Model):
 
     @classmethod
     def create_link(cls, long_url, expire_date):
-        bts = os.urandom(4)
+        bts = os.urandom(cls.NUMBER_OF_RANDOM_BYTES_FOR_SHORT_URL)
         short_url = b32encode(bts).decode('utf-8')
         link = Link.query.filter_by(short_url=short_url).first()
         link_expired = link.is_link_expired() if link else False
